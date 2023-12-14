7f21ddf450beecd517b71437134575977eb36577
Andy Chow <hohin523work@gmail.com>
2023-12-13 21:06:45 -0800

    Support hot reloading

--- web/next.config.js
+++ web/next.config.js
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  webpack: (config) => {
+    config.devServer = config.devServer || {};
+    config.devServer.watchOptions = {
+      poll: 1000,
+      aggregateTimeout: 300,
+    };
+    return config;
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
