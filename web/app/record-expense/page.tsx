ac020864280853ca823ae6c01d1f27d96c051f52
Andy Chow <hohin523work@gmail.com>
2023-12-30 21:34:29 -0800

    Create Basic Layout for the form

--- web/app/record-expense/page.tsx
+++ web/app/record-expense/page.tsx
@@ -1,9 +1,19 @@
-import React from 'react'
+import React from "react";
 
 const RecordExpense = () => {
   return (
-    <div>RecordExpense</div>
-  )
-}
+    <div className="h-screen p-40 bg-slate-200">
+      <div className="border rounded bg-white p-10">
+        <h2 className="text-base font-semibold leading-7 text-gray-900">
+          Add Expense Record
+        </h2>
+        <p className="mt-1 text-sm leading-6 text-gray-600">
+          This information will be displayed publicly so be careful what you
+          share.
+        </p>
+      </div>
+    </div>
+  );
+};
 
-export default RecordExpense
+export default RecordExpense;
