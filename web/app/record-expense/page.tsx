86ed1d437252bcc57f1de1588869da331dc99e87
Andy Chow <hohin523work@gmail.com>
2024-01-03 20:05:58 -0800

    fixed Amount input field

--- web/app/record-expense/page.tsx
+++ web/app/record-expense/page.tsx
@@ -36,12 +36,10 @@ export default function RecordExpense() {
               <div className="mt-2">
                 <div className="flex rounded-md px-3 shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md">
                   <input
-                    type="text"
-                    name="username"
-                    id="username"
-                    autoComplete="username"
+                    type="number"
+                    name="amount"
+                    id="amount"
                     className="block flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
-                    placeholder="janesmith"
                   />
                   <span className="flex select-none items-center pl-3 text-gray-500 sm:text-sm">
                     CAD
