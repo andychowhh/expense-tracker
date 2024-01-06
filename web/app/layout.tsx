47225e7df910f9d671c726b28a12682860c068a7
Andy Chow <hohin523work@gmail.com>
2024-01-05 19:19:30 -0800

    Created generic Modal

--- web/app/layout.tsx
+++ web/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import { Navbar } from "@/components";
+import { Navbar, Modal } from "@/components";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -18,6 +18,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
+        <Modal />
         <Navbar />
         {children}
       </body>
