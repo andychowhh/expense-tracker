3603d53bf7a37a3c6136834b2f1042cf2b3918a2
Andy Chow <hohin523work@gmail.com>
2024-02-02 21:00:52 -0800

    Add user field for Transaction

--- backend/src/transactions/interfaces/transaction.interface.ts
+++ backend/src/transactions/interfaces/transaction.interface.ts
@@ -1,5 +1,5 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
-import { now } from 'mongoose';
+import { now, Types, Schema as MongooseSchema } from 'mongoose';
 
 @Schema()
 export class Transaction {
@@ -20,6 +20,9 @@ export class Transaction {
 
   @Prop({ default: now() })
   updatedAt: Date;
+
+  @Prop({ type: MongooseSchema.Types.ObjectId, ref: 'User' })
+  user: Types.ObjectId;
 }
 
 export const TransactionSchema = SchemaFactory.createForClass(Transaction);
