8e949bbbe52a702dde3019ce2b8ad80ad5d0a12d
Andy Chow <hohin523work@gmail.com>
2024-03-13 21:28:10 -0700

    Update context provider

--- web/context/UserContext.tsx
+++ web/context/UserContext.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { ReactNode, useState, Dispatch } from "react";
+import React, { ReactNode, useState, Dispatch, useEffect } from "react";
 import { User } from "../types";
 
 interface UserContextProp {
@@ -11,13 +11,32 @@ interface UserContextProp {
 export const UserContext = React.createContext<UserContextProp | null>(null);
 
 export function UserContextProvider({
+  accessToken,
   children,
-  value,
 }: {
+  accessToken: string;
   children: ReactNode;
-  value: User;
 }) {
-  const [user, setUser] = useState<User>(value);
+  // TODO Remove
+  // const user = await axios.get("http://backend:3001/auth/me", {
+  //   params: {
+  //     jwtToken: await getCookie("accessToken"),
+  //   },
+  // });
+
+  useEffect(() => {
+    async function fetchUserProfile() {
+      const userProfileRaw = await fetch(
+        `${process.env.NEXT_PUBLIC_WEB_URL}/api/auth/me?jwtToken=${accessToken}`
+      );
+      const userRes = await userProfileRaw.json();
+      setUser(userRes);
+    }
+    fetchUserProfile();
+  }, [accessToken]);
+
+  const [user, setUser] = useState<User>({});
+
   return (
     <UserContext.Provider value={{ user, setUser }}>
       {children}
