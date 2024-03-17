fa585259cc08c32e041978ffe6cdeb55134d5fc2
Andy Chow <hohin523work@gmail.com>
2024-03-16 23:18:51 -0700

    Fixed

--- web/app/transactions/page.tsx
+++ web/app/transactions/page.tsx
@@ -1,15 +1,12 @@
 import { TransactionList } from "../../components/TransactionsList";
-import { TransactionListHeader, TransactionTable } from "../../components";
+import { TransactionListHeader } from "../../components";
 import { SearchParams } from "../../types";
 
-export default function TransactionsPage({
-  searchParams,
-}: {
-  searchParams?: SearchParams;
-}) {
+export default function TransactionsPage() {
   return (
-    <div className="">
-      <TransactionTable />
+    <div className="mt-5 mx-auto bg-white rounded sm:border sm:border-1 sm:max-w-md md:max-w-xl lg:max-w-3xl">
+      <TransactionListHeader />
+      <TransactionList />
     </div>
   );
 }
