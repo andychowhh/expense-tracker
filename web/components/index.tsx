e8ae944c660faadc0825a406bfcdb8c3c3f6edee
Andy Chow <hohin523work@gmail.com>
2024-03-19 21:51:45 -0700

    Cleanup component

--- web/components/index.tsx
+++ web/components/index.tsx
@@ -3,7 +3,5 @@ export * from "./CategorySelect";
 export * from "./CategorySelectGrid";
 export * from "./ExpenseStackedList";
 export * from "./Navbar";
-// export * from "./TransactionsList";
 export * from "./TransactionListHeader";
-export * from "./TransactionTable";
 export * from "./SideBar";
