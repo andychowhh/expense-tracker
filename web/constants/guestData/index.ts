66200a0dfa3c6b7a785203a533025d18619850b0
Andy Chow <hohin523work@gmail.com>
2024-03-23 16:06:51 -0700

    guestData

---
+++ web/constants/guestData/index.ts
@@ -0,0 +1,39 @@
+import { Transaction } from "@/types";
+
+export const guestTransactions: Transaction[] = [
+  {
+    _id: "1",
+    amount: 57,
+    category: "food",
+    date: "2024-03-23",
+    note: "Fried Rice",
+  },
+  {
+    _id: "2",
+    amount: 10,
+    category: "snack",
+    date: "2024-03-23",
+    note: "Chocolate",
+  },
+  {
+    _id: "3",
+    amount: 5,
+    category: "transportation",
+    date: "2024-03-23",
+    note: "Bus",
+  },
+  {
+    _id: "4",
+    amount: 500,
+    category: "shopping",
+    date: "2024-03-23",
+    note: "Jacket",
+  },
+  {
+    _id: "5",
+    amount: 25,
+    category: "entertainment",
+    date: "2024-03-23",
+    note: "Badminton",
+  },
+];
---
+++ web/utils/isGuest/index.ts
@@ -0,0 +1,7 @@
+import { cookies } from "next/headers";
+
+export const isGuest = () => {
+  const cookieStore = cookies();
+  const accessToken = cookieStore.get("accessToken");
+  return accessToken?.value === "";
+};
