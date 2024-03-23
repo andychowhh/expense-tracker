9d633334f4d77b3b07ad65899562ee4b742d912d
Andy Chow <hohin523work@gmail.com>
2024-03-23 16:30:42 -0700

    Fixed typo

--- web/components/AddNewRecordModal/index.tsx
+++ web/components/AddNewRecordModal/index.tsx
@@ -52,7 +52,7 @@ function SubmitButton({ disabled }: { disabled?: boolean }) {
     <button
       type="submit"
       disabled={pending || disabled}
-      className="inline-flex w-full justify-center rounded-md bg-red-600 px-3 py-2 text-sm font-semibold text-white shadow-sm disabled:bg-red-300 disabld: cursor-not-allowed hover:bg-red-500 sm:ml-3 sm:w-auto"
+      className="inline-flex w-full justify-center rounded-md bg-red-600 px-3 py-2 text-sm font-semibold text-white shadow-sm disabled:bg-red-300 disabled:cursor-not-allowed hover:bg-red-500 sm:ml-3 sm:w-auto"
     >
       {pending ? "Adding" : "Add"}
     </button>
