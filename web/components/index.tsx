daba40ae9db86570d3679e980245b02ed56b18e3
Andy Chow <hohin523work@gmail.com>
2024-03-31 20:02:35 -0700

    Cleanup

--- web/components/index.tsx
+++ web/components/index.tsx
@@ -1,5 +1,4 @@
 export * from "./CategorySelect";
 export * from "./CategorySelectGrid";
-export * from "./ExpenseStackedList";
 export * from "./Navbar";
 export * from "./SideBar";
--- web/constants/index.ts
+++ web/constants/index.ts
@@ -1,3 +1,2 @@
 export * from "./categories";
-export * from "./date";
 export * from "./navigation";
