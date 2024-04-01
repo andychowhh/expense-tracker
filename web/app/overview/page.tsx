6ccbb434121a9123197a683066bdb65cffb80abb
Andy Chow <hohin523work@gmail.com>
2024-03-31 22:48:27 -0700

    Set up Chart

---
+++ web/app/overview/page.tsx
@@ -0,0 +1,9 @@
+import { CategoryDonutChart } from "@/components/Overview";
+
+export default function OverviewPage() {
+  return (
+    <div className="w-6/12 h-6/12">
+      <CategoryDonutChart />
+    </div>
+  );
+}
