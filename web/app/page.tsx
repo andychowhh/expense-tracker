30b5d786667e78c9dce26d155fe5af044319228d
Andy Chow <hohin523work@gmail.com>
2024-04-19 18:48:59 -0700

    Fix import

--- web/app/page.tsx
+++ web/app/page.tsx
@@ -1,15 +1,3 @@
-import {
-  BudgetTracking,
-  Overview,
-  RecentTransactions,
-} from "../components/Home";
-
 export default function Home() {
-  return (
-    <div className="flex flex-col gap-7 pt-5">
-      <Overview />
-      <RecentTransactions />
-      <BudgetTracking />
-    </div>
-  );
+  return <div className="flex flex-col gap-7 pt-5">Home</div>;
 }
