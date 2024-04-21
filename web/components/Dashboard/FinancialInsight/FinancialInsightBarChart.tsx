dbe391adb51d56a781864fe47e3788b61256e995
Andy Chow <hohin523work@gmail.com>
2024-04-20 17:02:49 -0700

    position for financial insight

--- web/components/Dashboard/FinancialInsight/FinancialInsightBarChart.tsx
+++ web/components/Dashboard/FinancialInsight/FinancialInsightBarChart.tsx
@@ -28,8 +28,8 @@ export const FinancialInsightBarChart = ({
         data={data}
         margin={{
           top: 5,
-          right: 30,
-          left: 20,
+          right: 0,
+          left: 0,
           bottom: 5,
         }}
       >
