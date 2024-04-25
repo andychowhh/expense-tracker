34a8386dd5d5e383125c1f8276a483f17cbabce8
Andy Chow <hohin523work@gmail.com>
2024-04-24 17:39:47 -0700

    Fix modal open issue

--- web/components/Link/MobilePageLinks/index.tsx
+++ web/components/Link/MobilePageLinks/index.tsx
@@ -37,50 +37,52 @@ export function MobilePageLinks() {
           await createTransation(props)
         }
       />
-      <div className="flex flex-col gap-2 px-2 pb-3 pt-2 text-gray-700">
-        {navigation.map((item) => (
-          <Disclosure.Button
-            key={item.name}
-            as="a"
-            href={item.href}
-            className={`w-full text-left hover:bg-gray-700 hover:text-white block rounded-md px-3 py-2 text-base font-medium ${
-              pathname === item.href ? "bg-gray-100 pointer-events-none" : ""
-            }`}
-          >
-            {item.name}
-          </Disclosure.Button>
-        ))}
-        <>
-          {isEmpty(user) ? (
-            <div className="px-3 py-2">
-              <LoginButton />
-            </div>
-          ) : (
-            <>
-              <button
-                key="add-transactions"
-                className="w-full text-left hover:bg-gray-700 hover:text-white block rounded-md px-3 py-2 text-base font-medium"
-                onClick={() => toggleAddNewRecordModal()}
-              >
-                Add Transactions
-              </button>
-              <Disclosure.Button
-                key="sign-out"
-                className="w-full text-left hover:bg-gray-700 hover:text-white block rounded-md px-3 py-2 text-base font-medium"
-                onClick={logout}
-              >
-                Sign Out
-              </Disclosure.Button>
-            </>
-          )}
-        </>
-      </div>
-      {!isEmpty(user) && (
-        <div className="flex items-center gap-2 border-t px-5 py-3">
-          <ProfileImage size={32} picture={user ? user.picture : ""} />
-          <span className="text-sm">{user?.email}</span>
+      <Disclosure.Panel className="border-y border-neutral-200 absolute z-10 bg-white w-full lg:hidden">
+        <div className="flex flex-col gap-2 px-2 pb-3 pt-2 text-gray-700">
+          {navigation.map((item) => (
+            <Disclosure.Button
+              key={item.name}
+              as="a"
+              href={item.href}
+              className={`w-full text-left hover:bg-gray-700 hover:text-white block rounded-md px-3 py-2 text-base font-medium ${
+                pathname === item.href ? "bg-gray-100 pointer-events-none" : ""
+              }`}
+            >
+              {item.name}
+            </Disclosure.Button>
+          ))}
+          <>
+            {isEmpty(user) ? (
+              <div className="px-3 py-2">
+                <LoginButton />
+              </div>
+            ) : (
+              <>
+                <Disclosure.Button
+                  key="add-transactions"
+                  className="w-full text-left hover:bg-gray-700 hover:text-white block rounded-md px-3 py-2 text-base font-medium"
+                  onClick={() => toggleAddNewRecordModal()}
+                >
+                  Add Transactions
+                </Disclosure.Button>
+                <Disclosure.Button
+                  key="sign-out"
+                  className="w-full text-left hover:bg-gray-700 hover:text-white block rounded-md px-3 py-2 text-base font-medium"
+                  onClick={logout}
+                >
+                  Sign Out
+                </Disclosure.Button>
+              </>
+            )}
+          </>
         </div>
-      )}
+        {!isEmpty(user) && (
+          <div className="flex items-center gap-2 border-t px-5 py-3">
+            <ProfileImage size={32} picture={user ? user.picture : ""} />
+            <span className="text-sm">{user?.email}</span>
+          </div>
+        )}
+      </Disclosure.Panel>
     </div>
   );
 }
