aefad6b38d377942fb0862626e1d18367999c3bd
Andy Chow <hohin523work@gmail.com>
2024-04-24 19:15:24 -0700

    Create custom hook to update queryParams

--- web/components/Categories/CategoryDropdown.tsx
+++ web/components/Categories/CategoryDropdown.tsx
@@ -3,6 +3,7 @@
 import React, { useContext } from "react";
 import Dropdown from "../Dropdown/Dropdown";
 import { CategoryChartContext } from "@/context/CategoryChartContext";
+import { useUpdateQueryParams } from "@/hooks/useUpdateQueryParams";
 
 const TRANSACTION_TYPE_OPTIONS = [
   {
@@ -18,12 +19,16 @@ const TRANSACTION_TYPE_OPTIONS = [
 export const CategoryDropdown = () => {
   const { transactionType, setTransactionType } =
     useContext(CategoryChartContext)!;
+  const { updateQueryParams } = useUpdateQueryParams();
 
   return (
     <Dropdown
       selected={transactionType}
       options={TRANSACTION_TYPE_OPTIONS}
-      onSelect={(opt) => setTransactionType(opt)}
+      onSelect={(opt) => {
+        setTransactionType(opt);
+        updateQueryParams("transaction_type", opt);
+      }}
       menuButtonStyle={{ boxShadow: "none" }}
     />
   );
