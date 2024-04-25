d2310282f7328ca28b9fa9f11c6ef99dddf1dfcf
Andy Chow <hohin523work@gmail.com>
2024-04-24 17:32:30 -0700

    fix UX

--- web/components/Link/MobilePageLinks/index.tsx
+++ web/components/Link/MobilePageLinks/index.tsx
@@ -7,9 +7,15 @@ import { LoginButton } from "@/components/Button";
 import { UserContext } from "@/context/UserContext";
 import { isEmpty } from "lodash";
 import ProfileImage from "@/components/Dropdown/ProfileDropdown/ProfileImage";
-import { useRouter } from "next/navigation";
+import { usePathname, useRouter } from "next/navigation";
+import { TransactionModal } from "@/components/TransactionModal";
+import useToggle from "beautiful-react-hooks/useToggle";
+import { Transaction } from "@/types";
+import { createTransation } from "@/actions/transactions";
 
 export function MobilePageLinks() {
+  const [isAddNewRecordModalOpen, toggleAddNewRecordModal] = useToggle();
+  const pathname = usePathname();
   const { user, setUser } = useContext(UserContext) ?? {};
   const router = useRouter();
 
@@ -23,24 +29,41 @@ export function MobilePageLinks() {
 
   return (
     <div>
+      <TransactionModal
+        defaultValues={null}
+        isOpen={isAddNewRecordModalOpen}
+        onClose={toggleAddNewRecordModal}
+        onSubmit={async (props: Omit<Transaction, "_id">) =>
+          await createTransation(props)
+        }
+      />
       <div className="flex flex-col gap-2 px-2 pb-3 pt-2 text-gray-700">
         {navigation.map((item) => (
           <Disclosure.Button
             key={item.name}
             as="a"
             href={item.href}
-            className="w-full text-left hover:bg-gray-700 hover:text-white block rounded-md px-3 py-2 text-base font-medium"
+            className={`w-full text-left hover:bg-gray-700 hover:text-white block rounded-md px-3 py-2 text-base font-medium ${
+              pathname === item.href ? "bg-gray-100 pointer-events-none" : ""
+            }`}
           >
             {item.name}
           </Disclosure.Button>
         ))}
-        <div>
+        <>
           {isEmpty(user) ? (
             <div className="px-3 py-2">
               <LoginButton />
             </div>
           ) : (
-            <div>
+            <>
+              <button
+                key="add-transactions"
+                className="w-full text-left hover:bg-gray-700 hover:text-white block rounded-md px-3 py-2 text-base font-medium"
+                onClick={() => toggleAddNewRecordModal()}
+              >
+                Add Transactions
+              </button>
               <Disclosure.Button
                 key="sign-out"
                 className="w-full text-left hover:bg-gray-700 hover:text-white block rounded-md px-3 py-2 text-base font-medium"
@@ -48,12 +71,12 @@ export function MobilePageLinks() {
               >
                 Sign Out
               </Disclosure.Button>
-            </div>
+            </>
           )}
-        </div>
+        </>
       </div>
       {!isEmpty(user) && (
-        <div className="flex items-center gap-2 border-t px-5 py-2">
+        <div className="flex items-center gap-2 border-t px-5 py-3">
           <ProfileImage size={32} picture={user ? user.picture : ""} />
           <span className="text-sm">{user?.email}</span>
         </div>
